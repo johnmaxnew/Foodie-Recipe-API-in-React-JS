ab4be9831279de5296398a82547c48d83bfcee55
johnmaxnew <forevermeticulous@gmail.com>
2021-10-02 19:06:05 +0300

    modified styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
+import './key'
+import axios from 'axios';
+import Button from '@mui/material/Button';
+
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Container from '@material-ui/core/Container';
+import Card from './components/Card'
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 function App() {
+  const [query, setquery] = useState("")
+  // var url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  var url = `https://api.edamam.com/search?q=${query}&app_id=bb998e7e&app_key=17e9fb108d9e4c5522d35e32034aa9ee&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  async function getRecipes(){
+    var result = await axios.get(url);
+    console.log(result.data);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <h3>
-          API
-        </h3>
-      </header>
+
+      <div className="main">
+        <header className="App-header">
+          <h3  className="header_title">Foodie Recipes</h3>
+          <form className="search" action="" method="GET">
+            <input type="text" className="search-input" placeholder="Search recipes..." name="title__icontains" id="id_title__icontains" value={query} onChange={(e) => setquery(e.target.value)}/>
+            <Button type="submit" variant="contained" onClick={getRecipes}>Search</Button>
+          </form>
+        </header>
+        <div className="content">
+
+          <Container maxWidth={false}>
+            <Box sx={{ flexGrow: 1 }}>
+              <Grid container spacing={2}>
+                  <Grid item xs={12} sm={6} md={4}>
+                      <Card/>
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={4}>
+                      <Card/>
+                  </Grid>
+                  <Grid item xs={12} sm={6} md={4}>
+                      <Card/>
+                  </Grid>
+              </Grid>
+            </Box>
+          </Container>
+
+        </div>
+      </div>
     </div>
   );
 }
