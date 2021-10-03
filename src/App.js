22c83f5eb901356d55dfb1052b9a2006a8bb08c9
johnmaxnew <forevermeticulous@gmail.com>
2021-10-03 23:55:03 +0300

    modified styling

--- src/App.js
+++ src/App.js
@@ -68,23 +68,32 @@ function App() {
           </Container>
 
 
-          <ImageList sx={{ width: 500, height: 450 }}>
-      {itemData.map((item) => (
-        <ImageListItem key={item.img}>
-          <img
-            src={`${item.img}?w=248&fit=crop&auto=format`}
-            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-            alt={item.title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={item.title}
-            subtitle={<span>by: {item.author}</span>}
-            position="below"
-          />
-        </ImageListItem>
-      ))}
-    </ImageList>
+            <ImageList sx={{ width: 500, height: 450 }}>
+              {itemData.map((item) => (
+                <ImageListItem key={item.img}>
+
+
+                  {/* <img
+                    src={`${item.img}?w=248&fit=crop&auto=format`}
+                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                    alt={item.title}
+                    loading="lazy"
+                  />
+                  <ImageListItemBar
+                    title={item.title}
+                    subtitle={<span>by: {item.author}</span>}
+                    position="below"
+                  /> */}
+
+                      {recipes.map(recipe => {
+                        return  <Card recipe={ recipe } />;
+                      })}
+
+
+
+                </ImageListItem>
+              ))}
+            </ImageList>
 
 
         </div>
